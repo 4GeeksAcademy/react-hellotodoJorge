f989667e0ec885477d92f46693edef713a3caa71
Jorge Sanchez Ortiz <jorgesoibz@gmail.com>
2025-06-19 12:12:31 +0000

    final

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,33 +1,56 @@
 import React from "react";
-import {useState,} from "react";
-//include images into your bundle
+import { useState, useEffect } from "react";
+
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+
 const Home = () => {
-	const [inputName, setInputName] = useState("")
-	 const [list, setList] = useState([
-        { name: "" },
-    ])
-   const handleDelete = (idToRemove) => {
-    setList(list.filter((item) => item.id !== idToRemove));
-  };
-   
-    function handleSubmit(e) {
-        e.preventDefault();
-        let newItem = {
-            name: inputName,
-            id: Date.now()
-        }
-        setList(prev => [...prev, newItem]);
-		setInputName(""); 
+    const [inputName, setInputName] = useState("");
+
+    const [list, setList] = useState([]);
+
+
+    const getUser = async () => {
+        let result = await fetch("https://playground.4geeks.com/todo/users/jorge_sanchez");
+        let data = await result.json();
+        setList(data.todos)
+    };
+
+    useEffect(() => { getUser() }, []);
+    console.log(list);
+
+    const postTodos = async (event) => {
+        event.preventDefault();
+        await fetch("https://playground.4geeks.com/todo/todos/jorge_sanchez", {
+            method: "POST",
+            body: JSON.stringify(
+                {
+                    "label": inputName,
+                    "is_done": false
+                }
+            ),
+            headers: { "Content-type": "application/json" }
+        });
+        getUser()
+    };
+
+    const handleDelete = async (id) =>{
+
+        await fetch (`https://playground.4geeks.com/todo/todos/${id}`,{
+            method: "DELETE",
+            headers:{ "Content-type":"application/json"}
+
+        })
+        getUser()
+
     }
 
 
-	return (
-		<div className="text-center container w-75 p-4">
+
+    return (
+        <div className="text-center container w-75 p-4">
             <h1>To Do List</h1>
-            <form className="border p-2" onSubmit={(e) => handleSubmit(e)}>
+            <form className="border p-2" onSubmit={(e) => postTodos(e)}>
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Tareas</label>
                     <input
@@ -38,38 +61,38 @@ const Home = () => {
                         onChange={(e) => setInputName(e.target.value)}
                         value={inputName}
                     />
-                    
+
                 </div>
-                
+
                 <button type="submit" className="btn btn-primary">Añadir</button>
             </form>
             <div className="container-cards">
                 {
-                    list.map((item, index) => {
+                    list.lenght!==0 ? list.map((item, index) => {
                         return (
-							<div className="container h4 pb-1 mb-2  border-bottom border-secondary">
-                            <article key={index} className="custom-card d-flex justify-content-between align-items-center" onClick={ () => handleDelete(item.id)}>
-                               
-                                <span className="custom-name">{item.name}</span>
-								<span
-									className="text-danger ms-3"
-									style={{ cursor: "pointer", fontWeight: "bold",marginLeft:"auto", }}
-									onClick={() => handleDelete(item.id)}
-									title="Eliminar"
-      >
-									❌
-								</span>
-                            </article>
-							</div>
+                            <div className="container h4 pb-1 mb-2  border-bottom border-secondary">
+                                <article key={index} className="custom-card d-flex justify-content-between align-items-center" onClick={() => handleDelete(item.id)}>
+
+                                    <span className="custom-name">{item.label}</span>
+                                    <span
+                                        className="text-danger ms-3"
+                                        style={{ cursor: "pointer", fontWeight: "bold", marginLeft: "auto", }}
+                                        onClick={() => handleDelete(item.id)}
+                                        title="Eliminar"
+                                    >
+                                        ❌
+                                    </span>
+                                </article>
+                            </div>
                         )
-                    })
+                    }): null
                 }
             </div>
         </div>
     );
 }
 
-	
+
 
 
 export default Home;
